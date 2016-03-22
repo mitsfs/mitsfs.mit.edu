2eb2879e0aa885d3a6c461eb6fc6366545ee5101
Pweaver (Paul Weaver) <pweaver@panjiva.com>
2016-03-22 12:46:12 -0400

    add minutes.html page

---
+++ plugins/minutes.py
@@ -0,0 +1,10 @@
+
+MINUTES = ["latest", "2014", "2013", "2012", "2011", "2010", "2009", "2008", "2007", "2006", "2005", "2004", "2003", "2002", "2001", "2000", "1999", "1992"]
+
+ORDER = 10
+
+
+def preBuildPage(site, page, context, data):
+    if page.path == "minutes.html":
+        context['minutes'] = MINUTES
+    return context, data
