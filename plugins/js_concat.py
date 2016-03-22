966fc6dcfde04ee6442e42cb50a50dd6d5926fa5
Pweaver (Paul Weaver) <pweaver@panjiva.com>
2016-03-22 16:34:03 -0400

    add a js concatenator to resuce javascripts files on scheduler

---
+++ plugins/js_concat.py
@@ -0,0 +1,38 @@
+import os
+from shutil import copyfile
+
+JS_FILES = {"calendar_js": {"src": "static/scheduler/calendar.js",
+            "dest": [
+                "scheduler/ical.js",
+                "scheduler/dhtmlxscheduler.js",
+                "scheduler/dhtmlxscheduler_multisource.js",
+                "scheduler/dhtmlxscheduler_recurring.js",
+                "scheduler/dhtmlxscheduler_ical.js",
+                "scheduler/dhtmlxscheduler_tooltip.js",
+                "scheduler/dhtmlxscheduler_mitsfs.js", ]}}
+
+JS_ASSETS = {}
+
+
+def preBuild(site):
+    global JS_ASSETS
+    js_root = os.path.join(site._path, "js")
+    for name, d in JS_FILES.items():
+        JS_ASSETS[name] = []
+        if site.config.get("debug"):
+            for file in d["dest"]:
+                static_js_path = os.path.join("static", file)
+                copyfile(os.path.join(js_root, file), os.path.join(site._path, static_js_path))
+                JS_ASSETS[name].append(os.path.join('/', static_js_path))
+        else:
+            with open(os.path.join(site._path, d["src"]), "w") as f:
+                JS_ASSETS[name].append(os.path.join('/', d["src"]))
+                for file in d["dest"]:
+                    with open(os.path.join(js_root, file), "r") as old:
+                        f.write(old.read())
+
+
+def preBuildPage(site, page, context, data):
+    context['js_assets'] = JS_ASSETS
+
+    return context, data
--- plugins/sass.py
+++ plugins/sass.py
@@ -1,7 +1,12 @@
 import os
 import pipes
 
+
 def preBuild(site):
+    if site.config.get("debug"):
+        options = "--debug-info"
+    else:
+        options = "-t compressed"
     os.system(
-        'sass -t compressed --update %s/scss:%s/css' % (pipes.quote(site.path), pipes.quote(site.paths['static']))
+        'sass %s --update %s/scss:%s/css' % (options, pipes.quote(site.path), pipes.quote(site.paths['static']))
     )
