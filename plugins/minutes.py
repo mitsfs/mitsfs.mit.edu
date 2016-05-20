222169f24ad8f547b6eded8de4cd444519592236
Pweaver (Paul Weaver) <pweaver@panjiva.com>
2016-05-19 22:43:19 -0400

    adds mintues htaccess and symlink creation to show minutes

--- plugins/minutes.py
+++ plugins/minutes.py
@@ -8,3 +8,11 @@ def preBuildPage(site, page, context, data):
     if page.path == "minutes.html":
         context['minutes'] = MINUTES
     return context, data
+
+
+import os
+
+
+def postBuild(site):
+    path = os.path.join(site.build_path, 'minutes/archive')
+    os.symlink("/mit/mitsfs/www/minutes", path)
